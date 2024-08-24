5d9be68e5694d71313008b15d7447269da7373a9
hetchaudhari123 <hetchaudhari61@gmail.com>
2024-08-24 16:01:01 +0530

    u6

--- src/services/apis_by_me.jsx
+++ src/services/apis_by_me.jsx
@@ -1,4 +1,5 @@
-const BASE_URL = process.env.REACT_APP_BASE_URL
+// const BASE_URL = process.env.REACT_APP_BASE_URL
+const BASE_URL = "https://studynotion2-iket.onrender.com"
 export const categories = {
     CATEGORIES_API: BASE_URL + "/course/get-all-categories/",
 }
