79e110a63d30382d61f94cdf6d13510a048bdc16
hetchaudhari123 <hetchaudhari61@gmail.com>
2024-08-24 16:32:19 +0530

    u11

--- server/index.js
+++ server/index.js
@@ -44,13 +44,14 @@ app.use(fileUpload({
 	tempFileDir:"/tmp",
 }));
 //routes
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // Replace with your frontend URL
-//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//     res.header('Access-Control-Allow-Credentials', 'true'); // Required for cookies
-//     next();
-// });
+app.use((req, res, next) => {
+    // res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // Replace with your frontend URL
+    res.header('Access-Control-Allow-Origin', 'https://study-notion2.vercel.app'); // Replace with your frontend URL
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Credentials', 'true'); // Required for cookies
+    next();
+});
 app.use("/api/v1/auth", userRoutes);
 app.use("/api/v1/profile", profileRoutes);
 app.use("/api/v1/course", courseRoutes);
