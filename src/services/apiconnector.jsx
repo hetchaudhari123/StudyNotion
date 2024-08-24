6721669b0f39960c5dacec279a9e71e1cc949fbb
hetchaudhari123 <hetchaudhari61@gmail.com>
2024-08-24 17:02:53 +0530

    u13

--- src/services/apiconnector.jsx
+++ src/services/apiconnector.jsx
@@ -1,6 +1,26 @@
+// import axios from "axios";
+// const axiosInstance = axios.create({
+//     withCredentials: true,
+    
+// });
+// const apiConnector = (method,url,bodyData,headers,params) => {
+//     return axiosInstance({
+//         method:`${method}`,
+//         url:`${url}`,
+//         data: bodyData ? bodyData : null,
+//         headers: headers ? headers: null,
+//         params: params ? params : null,
+//     })
+// }
+// export default apiConnector
 import axios from "axios";
 const axiosInstance = axios.create({
-    withCredentials: true
+    withCredentials: true,
+    headers: {
+        'Content-Type': 'application/json',
+        // Add any other default headers here
+    }
+    
 });
 const apiConnector = (method,url,bodyData,headers,params) => {
     return axiosInstance({
