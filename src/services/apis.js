b0059397320dde01dad7f0962ede18c354f26299
hetchaudhari123 <hetchaudhari61@gmail.com>
2024-12-22 10:28:58 +0530

    36 commit

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,4 +1,5 @@
-const BASE_URL = process.env.REACT_APP_BASE_URL
+// const BASE_URL = process.env.REACT_APP_BASE_URL
+const BASE_URL = process.env.BASE_URL_OF_APP
 // const BASE_URL = "https://studynotion2-iket.onrender.com/api/v1"
 // const BASE_URL = "http://localhost:4000/api/v1"
 
