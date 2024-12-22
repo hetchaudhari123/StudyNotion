bd7706bb3c732c1379b86f03aa950f75ab57d9cd
hetchaudhari123 <hetchaudhari61@gmail.com>
2024-12-22 05:52:14 +0530

    22 commit

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,3 +1,103 @@
+// const jwt = require("jsonwebtoken");
+// require('dotenv').config();
+// exports.auth = async (req,res,next) => {
+//     try{
+
+//         //fetch the token
+//         // const token = req.cookies.token || req.body.token ||
+//         // req?.header("Authorization")?.replace("Bearer ","");
+//         // const token = req?.body?.Authorization?.replace("Bearer ","")
+//         const token = req?.body?.Authorization?.replace("Bearer ","") || req?.headers?.authorization?.replace("Bearer ", "");
+
+//         //check if it exists
+
+//         if(!token){
+//             console.log("req cookies",req.cookies);
+//             console.log("req ",req);
+//             console.log("req headers", req.headers);
+//             console.log("req body", req.body);
+//             return res.status(401).json({
+//                 success:false,
+//                 message:`Token missing req cookies:${req.cookies}`
+//             })
+//         }
+//         //verfiy
+
+//         try{
+//             // const decode = jwt.verify(token,process.env.JWT_SECRET);
+//             const decode = jwt.verify(token,"chaudhari");
+//             // console.log("DECODING THE TOKEN.....",decode);
+//             req.user = decode;
+//         }catch(err){
+//             console.log("ERROR FROM VERIFICATION OF THE TOKEN.......",err)
+//             return res.status(401).json({
+//                 success:false,
+//                 message:"Token is invalid"
+//             })
+//         }
+//         next();
+// }catch(err){
+//     console.log("ERROR FROM THE AUTH MIDDLEWARE",err);
+//     return res.status(401).json({
+//         success:false,
+//         message:`Error occurred while validating the token:`,
+//         error:err.message
+//     })
+// }
+    
+// }
+// exports.isStudent = async (req,res,next) => {
+//     try{
+//         if(req.user.accountType !== "Student"){
+//             return res.status(401).json({
+//                 success:false,
+//                 message:"The user is not a student."
+//             })
+//         }
+//         next();
+//     }catch(err){
+//         return res.status(500).json({
+//             success:false,
+//             message:"Error occurred while authorizing for the student."
+//         })
+//     }
+// }
+// exports.isInstructor = async (req,res,next) => {
+//     try{
+//         if(req.user.accountType !== "Instructor"){
+//             return res.status(401).json({
+//                 success:false,
+//                 message:"The user is not an instructor."
+//             })
+//         }
+//         next();
+//     }catch(err){
+//         return res.status(500).json({
+//             success:false,
+//             message:"Error occurred while authorizing the instructor."
+//         })
+//     }
+// }
+// exports.isAdmin = async (req,res,next) => {
+//     try{
+//         // console.log("Printing AccountType ", req.user.accountType);
+//         if(req.user.accountType !== "Admin"){
+//             return res.status(401).json({
+//                 success:false,
+//                 message:"The user is not an admin."
+//             })
+//         }
+//         next();
+//     }catch(err){
+//         return res.status(500).json({
+//             success:false,
+//             message:"Error occurred while authorizing the admin."
+//         })
+//     }
+// }
+
+
+
 const jwt = require("jsonwebtoken");
 require('dotenv').config();
 exports.auth = async (req,res,next) => {
@@ -5,26 +105,26 @@ exports.auth = async (req,res,next) => {
 
         //fetch the token
         // const token = req.cookies.token || req.body.token ||
-        // req?.header("Authorization")?.replace("Bearer ","");
-        // const token = req?.body?.Authorization?.replace("Bearer ","")
+        // req?.header("authorization")?.replace("Bearer ","");
+        // const token = req?.body?.Authorization?.replace("Bearer ","") 
         const token = req?.body?.Authorization?.replace("Bearer ","") || req?.headers?.authorization?.replace("Bearer ", "");
-
         //check if it exists
-
+        // console.log("The body inside the middleware is...",req?.body)
+        console.log("The body inside the middleware is...",req?.headers)
+        console.log("The token inside the middleware is...",token)
         if(!token){
-            console.log("req cookies",req.cookies);
-            console.log("req ",req);
-            console.log("req headers", req.headers);
-            console.log("req body", req.body);
+            // console.log("req cookies",req.cookies);
+            console.log("req inside",req);
             return res.status(401).json({
                 success:false,
-                message:`Token missing req cookies:${req.cookies}`
+                message:`Token missing is HERE ${req.cookies}`
             })
         }
         //verfiy
 
         try{
             // const decode = jwt.verify(token,process.env.JWT_SECRET);
+            console.log("The token obtained is...",token)
             const decode = jwt.verify(token,"chaudhari");
             // console.log("DECODING THE TOKEN.....",decode);
             req.user = decode;
--- src/components/core/Video/LectureVideo.jsx
+++ src/components/core/Video/LectureVideo.jsx
@@ -1,7 +1,6 @@
-import React, { useEffect,useState } from 'react'
-// import { Player } from 'video-react'
-import { BigPlayButton, Player } from "video-react"
-import "video-react/dist/video-react.css"
+import React, { useEffect, useState } from 'react'
+// import { BigPlayButton, Player } from "video-react"
+// import "video-react/dist/video-react.css"
 
 // import "node_modules/video-react/dist/video-react.css";
 import { useLocation, useNavigate, useParams } from 'react-router-dom';
@@ -14,62 +13,61 @@ import toast from 'react-hot-toast';
 import { addCompletedVideos, setCompletedVideos } from '../../../redux/slices/courseProgressSlice';
 const LectureVideo = () => {
     const dispatch = useDispatch()
-    const {subSectionId} = useParams()
-    const {sectionId} = useParams()
-    const {courseId} = useParams()
+    const { subSectionId } = useParams()
+    const { sectionId } = useParams()
+    const { courseId } = useParams()
     const videoRef = useRef(null)
     const location = useLocation()
-    const {courseDetails} = useSelector(state => state.course)
-    const [lecture,setLecture] = useState(null)
-    const [loading,setLoading] = useState(true)
-    const [prevSubSection,setPrevSubSection] = useState(null)
-    const [prevSection,setPrevSection] = useState(null)
-    const [nextSubSection,setNextSubSection] = useState(null)
-    const [nextSection,setNextSection] = useState(null)
-    const [endModal,setEndModal] = useState(false)
+    const { courseDetails } = useSelector(state => state.course)
+    const [lecture, setLecture] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const [prevSubSection, setPrevSubSection] = useState(null)
+    const [prevSection, setPrevSection] = useState(null)
+    const [nextSubSection, setNextSubSection] = useState(null)
+    const [nextSection, setNextSection] = useState(null)
+    const [endModal, setEndModal] = useState(false)
     const navigate = useNavigate()
-    const {completedVideos} = useSelector(state => state.courseProgress)
+    const { completedVideos } = useSelector(state => state.courseProgress)
 
-    const {token} = useSelector(state=>state.auth);
- 
+    const { token } = useSelector(state => state.auth);
+
 
 
     useEffect(() => {
         const fetch = () => {
             courseDetails.courseContent.forEach(section => {
-                 section.subSection.forEach(ss => {
-                     if(ss._id === subSectionId){
+                section.subSection.forEach(ss => {
+                    if (ss._id === subSectionId) {
                         //  console.log("THE LECTURE....",ss)
-                         setLecture(ss)
-                     }
-                 })
+                        setLecture(ss)
+                    }
+                })
             })
-         }
+        }
         const showPrevHandler = () => {
-            for(let i = 0 ; i < courseDetails.courseContent.length ; i++){
-                for(let j = 0 ; j < courseDetails.courseContent[i].subSection.length ; j++){
-                    if(courseDetails.courseContent[i].subSection[j]._id === subSectionId){
-                        if(j > 0){
+            for (let i = 0; i < courseDetails.courseContent.length; i++) {
+                for (let j = 0; j < courseDetails.courseContent[i].subSection.length; j++) {
+                    if (courseDetails.courseContent[i].subSection[j]._id === subSectionId) {
+                        if (j > 0) {
                             setPrevSubSection(courseDetails.courseContent[i].subSection[j - 1]._id)
                             setPrevSection(courseDetails.courseContent[i]._id)
                             return
                             // setPrevSection(courseDetails.courseContent[i][j - 1]._id)
                             // return courseDetails.courseContent[i][j - 1]._id
                         }
-                        else if(i > 0){
+                        else if (i > 0) {
                             setPrevSubSection(courseDetails.courseContent[i - 1].subSection[courseDetails.courseContent[i - 1].subSection.length - 1]._id)
                             setPrevSection(courseDetails.courseContent[i - 1]._id)
                             return
                             // return courseDetails.courseContent[i - 1][courseDetails.courseContent[i - 1].length - 1]._id
                         }
-                        else
-                        {
+                        else {
                             setPrevSubSection(null)
                             setPrevSection(null)
                             return
                             // return null
-                        } 
-                        
+                        }
+
                     }
                 }
             }
@@ -79,29 +77,28 @@ const LectureVideo = () => {
             // return null
         }
         const showNextHandler = () => {
-            
-            for(let i = 0 ; i < courseDetails.courseContent.length ; i++){
-                for(let j = 0 ; j < courseDetails.courseContent[i].subSection.length ; j++){
-                    if(courseDetails.courseContent[i].subSection[j]._id === subSectionId){
-                        if(j < courseDetails.courseContent[i].subSection.length - 1){
+
+            for (let i = 0; i < courseDetails.courseContent.length; i++) {
+                for (let j = 0; j < courseDetails.courseContent[i].subSection.length; j++) {
+                    if (courseDetails.courseContent[i].subSection[j]._id === subSectionId) {
+                        if (j < courseDetails.courseContent[i].subSection.length - 1) {
                             setNextSubSection(courseDetails.courseContent[i].subSection[j + 1]._id)
                             setNextSection(courseDetails.courseContent[i]._id)
                             return
                         }
-                        else if(i < courseDetails.courseContent.length - 1){
+                        else if (i < courseDetails.courseContent.length - 1) {
                             setNextSubSection(courseDetails.courseContent[i + 1].subSection[0]._id)
                             setNextSection(courseDetails.courseContent[i + 1]._id)
                             return
                             // return courseDetails.courseContent[i - 1][courseDetails.courseContent[i - 1].length - 1]._id
                         }
-                        else
-                        {
+                        else {
                             setNextSubSection(null)
                             setNextSection(null)
                             return
                             // return null
-                        } 
-                        
+                        }
+
                     }
                 }
             }
@@ -109,22 +106,22 @@ const LectureVideo = () => {
             // return null
             setNextSubSection(null)
             setNextSection(null)
-            return 
+            return
         }
 
         const showMarkAsCompletedHandler = () => {
             completedVideos.includes(subSectionId)
         }
 
-        if(courseDetails && completedVideos){
+        if (courseDetails && completedVideos) {
             fetch()
             showPrevHandler()
             showNextHandler()
             showMarkAsCompletedHandler()
             setLoading(false)
         }
-    },[completedVideos,courseDetails])
-  
+    }, [completedVideos, courseDetails])
+
     // useEffect(() => {
     //     console.log("SUB SECTION ID....",subSectionId)
     //     console.log("PREV SECTION......",prevSection)
@@ -132,24 +129,24 @@ const LectureVideo = () => {
     //     console.log("NEXT SECTION......",nextSection)
     //     console.log("NEXT SUB SECTION......",nextSubSection)
     // },[prevSection,nextSection,prevSubSection,nextSubSection])
-   
+
 
     const prevHandler = () => {
-        if(prevSection && prevSubSection){
+        if (prevSection && prevSubSection) {
             navigate(`/view-course/${courseId}/section/${prevSection}/sub-section/${prevSubSection}`)
             setEndModal(false)
         }
     }
     const nextHandler = () => {
-        if(nextSection && nextSubSection){
+        if (nextSection && nextSubSection) {
             navigate(`/view-course/${courseId}/section/${nextSection}/sub-section/${nextSubSection}`)
             setEndModal(false)
 
         }
     }
-    
+
     const rewatchHandler = () => {
-        if(videoRef){
+        if (videoRef) {
             videoRef.current.seek(0)
             setEndModal(false)
         }
@@ -170,7 +167,7 @@ const LectureVideo = () => {
         // })
         const section = courseDetails.courseContent.find(section => section._id === sectionId)
         const subSection = section.subSection.find(ss => ss._id === subSectionId)
-        const courseProgress = await updateCourseProgress({courseId,subSection},token,setLoading,true)
+        const courseProgress = await updateCourseProgress({ courseId, subSection }, token, setLoading, true)
         // console.log("COURSE PROGRESS COMPLETED VIDEOS....",courseProgress)
         // console.log("COMPLETED VIDEOS FROM THE UPDATE COURSE PROGRESS API....",courseProgress.completedVideos)
         dispatch(setCompletedVideos(courseProgress.completedVideos))
@@ -179,41 +176,41 @@ const LectureVideo = () => {
     }
 
 
-  return (
-  (!courseDetails || !completedVideos || loading) ? (
-    <div className='absolute top-0 bottom-0 left-0 right-0'>
-        <Spinner></Spinner>
-    </div>
-  ) : (
-    <div
-    //  style={{ width: 'calc(100% - )' }}
-     className='
+    return (
+        (!courseDetails || !completedVideos || loading) ? (
+            <div className='absolute top-0 bottom-0 left-0 right-0'>
+                <Spinner></Spinner>
+            </div>
+        ) : (
+            <div
+                //  style={{ width: 'calc(100% - )' }}
+                className='
      w-[100%] md:w-[80%]'>
-        <div className=' '>
-         <Player
-      playsInline
-    aspectRatio='16:9'
-        ref={videoRef}
-      src={lecture.videoUrl}
-      onEnded={() => setEndModal(true)}
-        fluid={true}
-    />
-        <div className=''>
-            {endModal && <VideoEndModal
-            onClickMarkAsCompleted={markAsCompletedHandler}
-            onClickPlayPrev={prevHandler}
-            onClickPlayNext={nextHandler}
-            onClickRewatch={rewatchHandler}
-            onClickClose={() => {setEndModal(false)}}
-            showMarkAsCompleted = {!completedVideos.includes(subSectionId)}
-            showPrev={prevSection && prevSubSection}
-            showNext={nextSection && nextSubSection}
-            />}
-        </div>
-        </div>
-     
-    </div>)
-  )
+                <div className=' '>
+                    {/* <Player
+                        playsInline
+                        aspectRatio='16:9'
+                        ref={videoRef}
+                        src={lecture.videoUrl}
+                        onEnded={() => setEndModal(true)}
+                        fluid={true}
+                    /> */}
+                    <div className=''>
+                        {endModal && <VideoEndModal
+                            onClickMarkAsCompleted={markAsCompletedHandler}
+                            onClickPlayPrev={prevHandler}
+                            onClickPlayNext={nextHandler}
+                            onClickRewatch={rewatchHandler}
+                            onClickClose={() => { setEndModal(false) }}
+                            showMarkAsCompleted={!completedVideos.includes(subSectionId)}
+                            showPrev={prevSection && prevSubSection}
+                            showNext={nextSection && nextSubSection}
+                        />}
+                    </div>
+                </div>
+
+            </div>)
+    )
 }
 
 export default LectureVideo
